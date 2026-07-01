0841e1ae3f57e70820d3fbdde897cf215493c390
Gemcasterr <supornthanaphat@gmail.com>
2026-07-01 16:58:13 +0700

    Create alpine1.fish

---
+++ alpine1.fish
@@ -0,0 +1,23 @@
+echo "Make sure all command are exactly Installed"
+echo "Procressing start!"
+
+sudo apk update && apk upgrade
+sudo apk add nvim
+
+cat > ~/.config/fish/config.fish << 'EOF'
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+export EDITOR="nvim"
+export fishconfig='~/.config/fish/config.fish'
+
+alias apkup="sudo apk update && apk upgrade"
+alias ..="cd .."
+alias fcon="$EDITOR $fishconfig"
+alias fload="source $fishconfig && echo 'Reloaded CONFIG fish'"
+alias cls="clear"
+EOF
+
+source ~/.config/fish/config.fish
+
+echo "End of procressing!"
